0dac575c7d73f3c6dd638849cd12b34733f44860
WilliamWJD <william.jose.wjd@gmail.com>
2020-08-24 20:27:31 -0300

    configurando TypeScript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello GoStack" });
+});
+
+app.listen(3333, () => {
+  console.log("✔ Server started on port 3333");
+});
