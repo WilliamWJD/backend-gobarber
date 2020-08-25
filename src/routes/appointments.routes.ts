b73bd6be7a9d8a66977a2a2ab8ec3c4648d23d41
williamwjd <william.jose.wjd@gmail.com>
2020-08-25 15:15:09 -0300

    regra que bloqueia a criação de agenamentos em horários iguais

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
     const { provider, date } = req.body;
 
-    const appointment = { id: uuid(), provider, date };
+    const parsedDate = startOfHour(parseISO(date));
+
+    // NÃO PERMITE A CRIAÇÃO DE UM AGENDAMENTO NO MESMO HORÁRIO
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate) {
+        return res
+            .status(400)
+            .json({ message: 'This appointment is already booked' });
+    }
+
+    const appointment = { id: uuid(), provider, date: parsedDate };
 
     appointments.push(appointment);
 
