6935eeea2a53ef1e4daa5a9605fbb1429fd8f2c4
WilliamWJD <william.jose.wjd@gmail.com>
2020-08-29 12:11:13 -0300

    gerando hash para senha do usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -9,6 +9,9 @@ usersRouter.post('/', async (req, res) => {
         const { name, email, password } = req.body;
         const createUser = new CreateUserService();
         const user = await createUser.execute({ name, email, password });
+
+        delete user.password;
+
         return res.json(user);
     } catch (err) {
         return res.status(400).json({ error: err.message });
