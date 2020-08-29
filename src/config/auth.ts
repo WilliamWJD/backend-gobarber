710c68291e94c845e152896ca3a302a552f0ced4
WilliamWJD <william.jose.wjd@gmail.com>
2020-08-29 14:05:29 -0300

    middleware de autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'cc2f040c791a0ce37544f5ab64299a47',
+        expiresIn: '1d',
+    },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -32,9 +34,9 @@ class AuthenticateUserService {
             throw Error('Incorrect email/password combination');
         }
 
-        const token = sign({}, 'cc2f040c791a0ce37544f5ab64299a47', {
+        const token = sign({}, authConfig.jwt.secret, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn: authConfig.jwt.expiresIn,
         });
 
         return { user, token };
