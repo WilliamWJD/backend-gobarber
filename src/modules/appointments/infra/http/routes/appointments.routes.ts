313cd5f1021d63c363b28205f10a896feb719444
williamwjd <william.jose.wjd@gmail.com>
2020-09-16 08:30:14 -0300

    separando responsabilidades das rotas para controllers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,33 +1,12 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentController from '../controllers/AppointmentController';
 
 const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (req, res) => {
-//     const appointments = await appointmentsRepository.find();
-//     return res.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (req, res) => {
-    const { provider_id, date } = req.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-        date: parsedDate,
-        provider_id,
-    });
-
-    return res.json(appointment);
-});
+appointmentsRouter.post('/', AppointmentController.create);
 
 export default appointmentsRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,43 +1,22 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
-
-import CreateUserService from '@modules/users/services/CreateUsersService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const createUser = container.resolve(CreateUserService);
-
-    const user = await createUser.execute({ name, email, password });
-
-    delete user.password;
-
-    return res.json(user);
-});
+usersRouter.post('/', UsersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (req, res) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-        const user = await updateUserAvatar.execute({
-            user_id: req.user.id,
-            avatar_filename: req.file.filename,
-        });
-
-        delete user.password;
-
-        return res.json(user);
-    },
+    UserAvatarController.update,
 );
 
 export default usersRouter;
