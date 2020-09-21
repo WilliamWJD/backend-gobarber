baf25bc6e5a54b2625eca2d97fb1dc976ecfe1fe
williamwjd <william.jose.wjd@gmail.com>
2020-09-21 15:54:01 -0300

    iniciando teste para recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordService.spec.ts
@@ -0,0 +1,31 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/Fakes/IUsersRepository';
+import SendForgotPasswordService from './SendForgotPasswordService';
+
+describe('SendForgotPasswordEmail', () => {
+    it('should be able to recover the password using the email', async () => {
+        const fakeUserRepository = new FakeUsersRepository();
+        const fakeMailProvider = new FakeMailProvider();
+
+        const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+        const sendForgotPassword = new SendForgotPasswordService(
+            fakeUserRepository,
+            fakeMailProvider,
+        );
+
+        await fakeUserRepository.create({
+            name: 'william',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        await sendForgotPassword.execute({
+            email: 'johndoe@example.com',
+        });
+
+        expect(sendMail).toHaveBeenCalled();
+    });
+});
