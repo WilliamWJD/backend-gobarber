4a00e4ceada135f6042c51169f1d2f0cc9880965
williamwjd <william.jose.wjd@gmail.com>
2020-09-21 13:44:45 -0300

    finalizando testes de autenticação de usuário

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeUserRepository from '../repositories/Fakes/IUsersRepository';
@@ -35,5 +35,50 @@ describe('AuthenticateUser', () => {
 
         expect(response).toHaveProperty('token');
         expect(response.user).toEqual(user);
+    });
+
+    it('should not be able to authenticate with non existing user', () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUserService = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+
+        expect(
+            authenticateUserService.execute({
+                email: 'william@william.com.br',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should be able to authenticate with wrong password', async () => {
+        const fakeUsersRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+
+        const authenticateUserService = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+
+        await createUser.execute({
+            name: 'William',
+            email: 'william@william.com.br',
+            password: '123456',
+        });
+
+        expect(
+            authenticateUserService.execute({
+                email: 'william@william.com.br',
+                password: 'wrong-password',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
