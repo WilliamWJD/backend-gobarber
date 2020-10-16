fd2dd0163968f6cdf2ef1f131659523a49159590
William <william.jose.wjd@gmail.com>
2020-10-16 10:52:38 -0300

    configurando conexao com o mongo

---
+++ ormconfig.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        "type": "postgres",
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "entities": [
+          "./src/modules/**/infra/typeorm/entities/*.ts"
+        ],
+        "migrations": [
+          "./src/shared/infra/typeorm/migrations/*.ts"
+        ],
+        "cli": {
+          "migrationsDir": "./src/shared/infra/typeorm/migrations"
+        }
+    },
+    {
+        "type": "mongodb",
+        "host": process.env.DB_MONGO_HOST,
+        "port": process.env.DB_MONGO_PORT,
+        "database": process.env.DB_MONGO_NAME,
+        "useUnifiedTopology":true,
+        "entities": [
+          "./src/modules/**/infra/typeorm/schemas/*.ts"
+        ],
+    }
+]
